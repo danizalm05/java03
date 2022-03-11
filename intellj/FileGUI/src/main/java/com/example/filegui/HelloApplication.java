858701763132da8ad80e1e06df67a4141fac676f
Richm <ric5hme45@yahoo.com>
2022-03-11 16:57:03 +0200

    2425

--- intellj/FileGUI/src/main/java/com/example/filegui/HelloApplication.java
+++ intellj/FileGUI/src/main/java/com/example/filegui/HelloApplication.java
@@ -1,7 +1,12 @@
 //16: FileChooser: loading files
 //https://www.youtube.com/watch?v=fvf1WOPqLBw&list=PLrzWQu7Ajpi26jZvP8JhEJgFPFEj_fojO&index=19
-//5:00
+//17: FileChooser: saving files
+//https://www.youtube.com/watch?v=ZGFjaZLwqns&list=PLrzWQu7Ajpi26jZvP8JhEJgFPFEj_fojO&index=20
+//Source code
 //https://gist.github.com/Da9el00/f75565592ded943420969a3fbcad4d2a
+
+
+
 
 
 package com.example.filegui;
--- intellj/FileGUI/src/main/java/com/example/filegui/HelloController.java
+++ intellj/FileGUI/src/main/java/com/example/filegui/HelloController.java
@@ -9,8 +9,10 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class HelloController implements Initializable {
     FileChooser fileChooser = new FileChooser();
@@ -34,6 +36,17 @@ public class HelloController implements Initializable {
     @FXML
     void getText(MouseEvent event) {
         File file = fileChooser.showOpenDialog(new Stage());
+
+        if(file != null){
+            try {
+                Scanner scanner = new Scanner(file);
+                while(scanner.hasNextLine()){
+                    textArea.appendText(scanner.nextLine() + "\n");
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
   @Override
