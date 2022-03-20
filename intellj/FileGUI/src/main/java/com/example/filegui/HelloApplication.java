3f3d15324c72ee3aa64bc6e44795b1ce7d2af708
Richm <ric5hme45@yahoo.com>
2022-03-20 15:43:52 +0200

    4258

--- intellj/FileGUI/src/main/java/com/example/filegui/HelloApplication.java
+++ intellj/FileGUI/src/main/java/com/example/filegui/HelloApplication.java
@@ -4,8 +4,17 @@
 //https://www.youtube.com/watch?v=ZGFjaZLwqns&list=PLrzWQu7Ajpi26jZvP8JhEJgFPFEj_fojO&index=20
 //Source code
 //https://gist.github.com/Da9el00/f75565592ded943420969a3fbcad4d2a
+/*Export JavaFX 11, 15 or 17 projects into an executable jar file with IntelliJ [2022]
+1. Create new file java class file  main_1.java
+2. Add the next code in  the  "class Main_1{} ":
+3. File > Project Structure > Artifects > Add >jar > From modulo with dependence
+   public static void main(String[] args){
 
 
+5. Build > Build Artifacts >build
+6 in the out  folder we will have the '*.jar' file
+   }
+ */
 
 
 
---
+++ intellj/FileGUI/src/main/java/com/example/filegui/Main_1.java
@@ -0,0 +1,8 @@
+package com.example.filegui;
+
+public class Main_1 {
+    public static void main(String[] args) {
+      //HelloApplication is the Main class
+        HelloApplication.main(args);
+    }
+}
